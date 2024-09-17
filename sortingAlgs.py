3459637c08862036477b5f4780a01890614f5fa2
ejml1 <liethanjm@gmail.com>
2024-09-17 09:29:32 +0800

    Merge Sort

--- sortingAlgs.py
+++ sortingAlgs.py
@@ -12,6 +12,38 @@ def bubble_sort(data, timeTick):
                 drawSwap(data, j, j + 1, i, timeTick)
         drawCorrectPositionGreaterEqualThanOuterLoop(data, i, timeTick)
 
+
+def merge_sort(data, timeTick):
+    merge_sort_alg(data, 0, len(data) - 1, timeTick)
+
+def merge_sort_alg(data, left, right, timeTick):
+    if left < right:
+        middle = (left + right) // 2
+        merge_sort_alg(data, left, middle, timeTick)
+        merge_sort_alg(data, middle + 1, right, timeTick)
+        merge(data, left, middle, right, timeTick)
+
+def merge(data, left, middle, right, timeTick):
+    leftPart = data[left:middle + 1]
+    rightPart = data[middle + 1: right + 1]
+
+    leftIdx = rightIdx = 0
+
+    for dataIdx in range(left, right + 1):
+        if leftIdx < len(leftPart) and rightIdx < len(rightPart):
+            if leftPart[leftIdx] <= rightPart[rightIdx]:
+                data[dataIdx] = leftPart[leftIdx]
+                leftIdx += 1
+            else:
+                data[dataIdx] = rightPart[rightIdx]
+                rightIdx += 1
+        elif leftIdx < len(leftPart):
+            data[dataIdx] = leftPart[leftIdx]
+            leftIdx += 1
+        else:
+            data[dataIdx] = rightPart[rightIdx]
+            rightIdx += 1
+
 ########################################################################################################################
 ### Drawing Methods ####################################################################################################
 
